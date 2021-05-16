67bf22f63ce9f7ba3e6075b57e500096d2b3544b
Will Watts <willw@applied.co.uk>
2021-05-16 12:04:42 +0100

    Complete UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,63 @@
 import React from 'react';
 import './App.css';
 
+import {TResult, getResults} from './calcs';
+
 type SubmitHandler = (e: React.SyntheticEvent) => void;
 
-function EntryForm({onSubmit } : {onSubmit : SubmitHandler}) {
+
+function EntryForm({ onSubmit }: { onSubmit: SubmitHandler }) {
+  const [validNumbers, setValidNumbers] = React.useState(false);
+
+  const totalRef = React.useRef<HTMLInputElement>(null);
+  const squareCountRef = React.useRef<HTMLInputElement>(null);
+
+  const handleChange = (/* e: React.ChangeEvent<HTMLInputElement> */) => {
+    const v =
+      (totalRef?.current?.valueAsNumber ?? 0) > 0 &&
+      (squareCountRef?.current?.valueAsNumber ?? 0) > 0;
+    setValidNumbers(v);
+  }
   return (
     <form onSubmit={onSubmit} className="will-style">
       <div>
         <div className="will-div">
           <label htmlFor="total">Total: </label>
-          <input name="total" type="number" />
+          <input ref={totalRef} onChange={handleChange} name="total" type="number" />
         </div>
         <div className="will-div">
           <label htmlFor="squareCount">Count of Squares: </label>
-          <input name="squareCount" type="number" />
+          <input ref={squareCountRef} onChange={handleChange} name="squareCount" type="number" />
         </div>
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={!validNumbers}>Submit</button>
     </form>
   )
 
 }
 
+function ResultDisplay({results} : {results: TResult | null}) {
+  return (
+    <div className="will-style" hidden={results === null} >
+      <p>Result:</p>
+      {results?.length === 0 ? 
+      (
+        <p style={{color:'red'}} >No Valid Results</p>
+      ) :  (
+        <ul>
+        {results?.map((item, index) => (
+          <li key={index}>{item.join(", ")}</li>
+        ))}
+        </ul>
+      )}
+    </div>
+  )
+}
 
 function App() {
-
   const [total, setTotal] = React.useState(0);
   const [squareCount, setSquareCount] = React.useState(0);
+  const [results, setResults] = React.useState<TResult | null>(null);
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -36,17 +67,15 @@ function App() {
     };
     setTotal(target.total.value);
     setSquareCount(target.squareCount.value);
+    const r = getResults(total, squareCount);
+    setResults(r);
   }
-  
-  React.useEffect(()=>{
-    if (total > 0 && squareCount > 0) {
-      alert(`You entered: ${total} and ${squareCount}`)
-    }
-  }, [total, squareCount]);
-  
 
   return (
-    <EntryForm onSubmit={handleSubmit}></EntryForm>
+    <>
+      <EntryForm onSubmit={handleSubmit}></EntryForm>
+      <ResultDisplay results={results}/>
+    </>
   );
 }
 
---
+++ src/calcs.ts
@@ -0,0 +1,8 @@
+
+export type TResult = Array<Array<number>>;
+
+
+export function getResults(total: number, squareCount: number): TResult {
+    // return [];
+    return[[1,2,3],[5,6,7]];
+}
